c57d7fe2c7cfeaae989e3a14f7f7540993462158
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-02-11 12:26:09 +0300

    Added EnglishLanguage

---
+++ Sources/Dictionary/Language/EnglishLanguage.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Olcay Taner YILDIZ on 11.02.2022.
+//
+
+import Foundation
+
+public class EnglishLanguage: Language{
+    
+    public static let LOWERCASE_LETTERS = "abcdefghijklmnopqrstuvwxyz"
+    public static let UPPERCASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+}
