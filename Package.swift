559b53e0b7552d53ec17498d51c7fb635abbfb44
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-04-30 11:38:56 +0300

    Updated Meth dependency.

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["Dictionary"]),
     ],
     dependencies: [
-        .package(name: "Math", url: "https://github.com/StarlangSoftware/Math-Swift.git", .exact("1.0.3")),
+        .package(name: "Math", url: "https://github.com/StarlangSoftware/Math-Swift.git", .exact("1.0.11")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
